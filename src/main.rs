1e60e945456ec0222dbcd7cd2be4fd252650bd0d
oichan <makoto.uwaya@gmail.com>
2024-03-23 10:23:19 +0900

    Add User struct sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);     // xの値は{}です
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let data = "user";
+
+    let guess: u32 = match "42".parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Not number");
+            return;
+        },
+    };
+    println!("{} da, {}", guess, data);
+    print_month(11);
+
+    let user1 = build_user(String::from("hoge h"), String::from("email@test.example"));
+    print_user_info(&user1);
+
+    let user2 = User {
+        username: String::from("aaaa bbbbb"),
+        email: String::from("saasdf@asdfkljasdf.example"),
+        ..user1
+    };
+    print_user_info(&user2);
+}
+
+fn print_month(index: usize) {
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    println!("{}", months[index])
+
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(username: String, email: String) -> User {
+    User {
+        username,
+        email,
+        sign_in_count: 1,
+        active: true,
+    }
+}
+
+fn print_user_info(user: &User) {
+    println!("username: {},
+     email:{} ({}) - {}", user.username, user.email, user.active, user.sign_in_count);
 }
