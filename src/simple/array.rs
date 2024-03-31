6d3cab43dc4adc1a5b9a6bf452d70dbf888d88e5
oichan <makoto.uwaya@gmail.com>
2024-03-31 19:37:24 +0900

    Add Read and Write Vec

--- src/simple/array.rs
+++ src/simple/array.rs
@@ -19,9 +19,31 @@ pub fn print_month(index: usize) {
 
 #[allow(dead_code)]
 pub fn using_vec() {
-    let mut vec: Vec<i32> = Vec::new();
+    let mut vec = Vec::new();
     vec.push(1);
     vec.push(2);
     vec.push(3);
+    let first = vec[0];
+    println!("first: {:?}", first);
     println!("{:?}", vec);
+    vec.push(4);
+    println!("first: {:?}", vec);
+    for i in &vec {
+        println!("four_information: {}", i);
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    let third = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(value) => println!("The third element is {}", value),
+        None => println!("There is no third element"),
+    }
+
+    for i in &mut v {
+        *i += 50;
+        // println!("{}", i);
+    }
+    println!("{:?}", v);
 }
